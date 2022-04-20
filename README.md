71c8b9fd19d0f4bc69dcd566c1767821346ad17a
Alex Byrnes <a.byrnes@northeastern.edu>
2022-04-20 11:12:47 -0400

    New readme section on building, version and clean commands in build script.

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-This is a crude start to the Cobey Lab Handbook using **bookdown** (https://github.com/rstudio/bookdown). 
+Cobey Lab Handbook using **bookdown** (https://github.com/rstudio/bookdown).
+
+
+# Building
+
+*Currently built with Bookdown 0.25.*
+
+``` bash
+
+./_build.sh
+
+```
+
--- _build.sh
+++ _build.sh
@@ -1,6 +1,16 @@
 #!/bin/sh
 
+echo "Clean up dangling files."
+Rscript -e "bookdown::clean_book(clean = getOption('bookdown.clean_book', TRUE))"
+
+echo "Build git book."
 Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::gitbook')"
+
+echo "Build pdf."
 Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::pdf_book')"
+
+echo "Build epub."
 Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::epub_book')"
 
+echo -n "Rendered with Bookdown "
+Rscript -e "packageVersion('bookdown')"
